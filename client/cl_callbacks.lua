6b36d995e41e475777cbd97201dff3bcf5af6f4c
Darr0k <96451713+Darr0k@users.noreply.github.com>
2023-05-13 02:47:17 +0300

    Update cl_callbacks.lua
    Build the client side

--- client/cl_callbacks.lua
+++ client/cl_callbacks.lua
@@ -1,1 +1,11 @@
+function Triggercallback(name, cb, ...)
+    Callbacks[name] = cb
+    TriggerServerEvent('Darrk/Server/Callback', name, ...)
+end
 
+RegisterNetEvent('Darrk/Client/Callback', function(name, ...)
+    if Callbacks[name] then
+        Callbacks[name](...)
+        Callbacks[name] = nil
+    end
+end)
