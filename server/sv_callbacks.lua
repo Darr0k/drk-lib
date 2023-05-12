c124027a89408a61609c3033bd9e459525134f6b
Darr0k <96451713+Darr0k@users.noreply.github.com>
2023-05-13 02:53:51 +0300

    Build the server side for the callbacks

--- server/sv_callbacks.lua
+++ server/sv_callbacks.lua
@@ -1,1 +1,11 @@
+function Triggercallback(name, source, cb, ...)
+    if not Callbacks[name] then return end
+    Callbacks[name](source, cb, ...)
+end
 
+RegisterNetEvent('Darrk/Server/Callback', function(name, ...)
+    local src = source
+    Triggercallback(name, src, function(...)
+        TriggerClientEvent('Darrk/Client/Callback', src, name, ...)
+    end, ...)
+end)
