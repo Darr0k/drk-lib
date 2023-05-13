64dc5dcd880cc1786b2b01abbf808c272142af9e
Darr0k <96451713+Darr0k@users.noreply.github.com>
2023-05-13 03:01:08 +0300

    Add a function to create a callback

--- server/sv_callbacks.lua
+++ server/sv_callbacks.lua
@@ -1,11 +1,15 @@
-function Functions.Triggercallback(name, source, cb, ...)
+function Functions.CreateCallback(name, cb)
+    Callbacks[name] = cb
+end
+
+function Functions.TriggerCallback(name, source, cb, ...)
     if not Callbacks[name] then return end
     Callbacks[name](source, cb, ...)
 end
 
 RegisterNetEvent('Darrk/Server/Callback', function(name, ...)
     local src = source
-    Triggercallback(name, src, function(...)
+    Functions.TriggerCallback(name, src, function(...)
         TriggerClientEvent('Darrk/Client/Callback', src, name, ...)
     end, ...)
 end)
